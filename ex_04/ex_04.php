2ca0bac4f8d6e9226429cedfa75e14292d1b586e
Elllssa <Elllssa>
2023-10-04 15:39:40 +0200

    ex 04

---
+++ ex_04/ex_04.php
@@ -0,0 +1,4 @@
+<?php
+function my_contact($str1, $str2){
+    echo ($str1. " " .  $str2);
+}
---
+++ ex_04/index.php
@@ -0,0 +1,4 @@
+<?php
+require("ex_04.php");
+my_contact("Hello", "world");
+my_contact("My name is", "Odin");
